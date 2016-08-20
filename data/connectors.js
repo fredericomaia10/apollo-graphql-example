82422acf3b6cc9b65da4af6e2458787dc9eac744
Frederico Maia Arantes <frederico.maia@tecsinapse.com.br>
2016-08-20 14:09:21 -0300

    Commit inicial

---
+++ data/connectors.js
@@ -0,0 +1,70 @@
+import Sequelize from 'sequelize';
+import casual from 'casual';
+import _ from 'lodash';
+import Mongoose from 'mongoose';
+import rp from 'request-promise';
+
+//SQL
+const db = new Sequelize('blog', null, null, {
+  dialect: 'sqlite',
+  storage: './blog.sqlite',
+});
+
+const AuthorModel = db.define('author', {
+  firstName: { type: Sequelize.STRING },
+  lastName: { type: Sequelize.STRING },
+});
+
+const PostModel = db.define('post', {
+  title: { type: Sequelize.STRING },
+  text: { type: Sequelize.STRING },
+});
+
+AuthorModel.hasMany(PostModel);
+PostModel.belongsTo(AuthorModel);
+
+//MongoDB
+const mongo = Mongoose.connect('mongodb://localhost/views');
+
+const ViewSchema = Mongoose.Schema({
+  postId: Number,
+  views: Number
+});
+
+const View = Mongoose.model('views', ViewSchema);
+
+//Rest
+const FortuneCookie = {
+  getOne() {
+    return rp('http://fortunecookieapi.com/v1/cookie')
+      .then((res) => JSON.parse(res))
+      .then((res) => {
+        return res[0].fortune.message;
+      })
+  }
+}
+
+casual.seed(123);
+db.sync({ force: true }).then(() => {
+  _.times(10, () => {
+    return AuthorModel.create({
+      firstName: casual.first_name,
+      lastName: casual.last_name,
+    }).then((author) => {
+      return author.createPost({
+        title: `A post by ${author.firstName}`,
+        text: casual.sentences(3),
+      });
+    }).then((post) => {
+      return View.update(
+        { postId: post.id },
+        { views: casual.integer(0, 100) },
+        { upsert: true });
+    })
+  });
+});
+
+const Author = db.models.author;
+const Post = db.models.post;
+
+export { Author, Post, View, FortuneCookie };
---
+++ data/resolvers.js
@@ -0,0 +1,30 @@
+import { Author } from './connectors';
+import { View } from './connectors';
+import { FortuneCookie } from './connectors';
+
+const resolvers = {
+  Query: {
+    author(_, args) {
+      return Author.find( { where: args });
+    },
+    getFortuneCookie() {
+      return FortuneCookie.getOne();
+    }
+  },
+  Author: {
+    posts(author){
+      return author.getPosts();
+    },
+  },
+  Post: {
+    author(post) {
+      return post.getAuthor();
+    },
+    views(post) {
+      return View.findOne({ postId: post.id }).
+              then((view) => view.views );
+    },
+  },
+};
+
+export default resolvers;
